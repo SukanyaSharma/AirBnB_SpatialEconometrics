1ab2b77bd2b0040d5018511bfafdee4eba08a642
vkarve2 <vkarve2@illinois.edu>
2018-03-07 15:44:41 -0600

    Edit Spatial_Lag.py to create a new LOG file each day.
    It also now takes in an arbitraru shapefile and an arbitrary dataset as input.

--- Spatial_Lag.py
+++ Spatial_Lag.py
@@ -1,51 +1,54 @@
+from __init__ import *
 import pysal as ps
 import numpy as np
 
-db = ps.open('Dataset_MAIN/enhanced_dataset.csv', 'r')
-print(list(enumerate(db.header)))
 
 
-rent_aff_1 = db.by_col('rent_aff_1')
-rent_aff_1 = list(map(float, rent_aff_1))
-y = np.array(rent_aff_1).reshape(len(db), 1); print('y = rent_aff_1')
+name_ds = 'Dataset_MAIN/enhanced_dataset.csv'
+shapefile = 'Listings shp/MAIN_CT_with_listings/MAIN_tracts_with_listings.shp'
+#name_y = 'rent_aff_3'
+name_y = 'log_median_rent'
+x_columns = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22]
 
 
-columns = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 21]
 
-x = np.zeros((len(db), 1))
 
-for i, c in enumerate(columns):
-    data = db.by_col(db.header[c])
+ds = ps.open(name_ds, 'r')
+print(list(enumerate(ds.header)))
+logger.info('Dataset = ' + name_ds)
+logger.info('Shapefile = ' + shapefile)
+
+y = ds.by_col(name_y)
+y = list(map(float, y))
+y = np.array(y).reshape(len(ds), 1)
+logger.info('y = ' + name_y)
+
+
+x = np.zeros((len(ds), 1))
+for i, c in enumerate(x_columns):
+    data = ds.by_col(ds.header[c])
     data = list(map(float, data))
-    data = np.array(data).reshape(len(db), 1)    
-    print('Variable #{} :'.format(i+1), db.header[c])
+    data = np.array(data).reshape(len(ds), 1)
+    logger.info('Variable #{} :'.format(i+1) + ds.header[c])
     x = np.hstack((x, data))
 x = x[:, 1:]
 
 
 
 # Weights, row-standarsized
-w = ps.knnW_from_shapefile('Listings shp/MAIN_CT_with_listings/MAIN_tracts_with_listings.shp', k=4, idVariable=None)
+w = ps.knnW_from_shapefile(shapefile, k=4, idVariable=None)
 w.transform = 'r'
 # Kernel-weights
-kw = ps.adaptive_kernelW_from_shapefile('Listings shp/MAIN_CT_with_listings/MAIN_tracts_with_listings.shp', k=12, idVariable=None)
+kw = ps.adaptive_kernelW_from_shapefile(shapefile, k=12, idVariable=None)
 
 
-
-
-
-
-
-
-logfile = open('Results.log', 'a')
 
 ## For Spatial Lag Model -- Maximum Likelihood
-ml_lag = ps.spreg.ML_Lag(y, x, w); print(ml_lag.summary); logfile.write('\n'*2 + ml_lag.summary + '\n'*2)
+logger.info('Method = Spatial Lag Model -- Maximum Likelihood')
+ml_lag = ps.spreg.ML_Lag(y, x, w, name_y = name_y, name_ds = name_ds)
+logger.info('\n'*2 + ml_lag.summary + '\n'*2)
 ## For Spatial Lag Model -- Maximum Likelihood, with Ord Method
-ml_lag_ord = ps.spreg.ML_Lag(y, x, w, method='ord'); print(ml_lag_ord.summary); logfile.write('\n'*2 + ml_lag_ord.summary + '\n'*2)
-
-logfile.close()
-
+#ml_lag_ord = ps.spreg.ML_Lag(y, x, w, method='ord'); print(ml_lag_ord.summary); logfile.write('\n'*2 + ml_lag_ord.summary + '\n'*2)
 
 
 
---
+++ __init__.py
@@ -0,0 +1,27 @@
+#  Logger for writing outputs to .log file.
+#  .info prints to standard output + .log file
+#  .debug prints to only .log file
+
+import logging
+import time
+import sys
+
+logdatetime = time.strftime("%m%d")
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.handlers =[]
+
+fh = logging.FileHandler('./'+logdatetime+'.log', mode='a')
+fh.setLevel(logging.DEBUG)
+
+sh = logging.StreamHandler(stream=sys.stdout)
+sh.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(sh)
+
+
